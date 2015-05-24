86f471a9f8bf915770e20afee41c270efa1a6fd8
Godrikxp <Godrik-xp@yandex.ru>
2015-05-25 01:51:00 +0500

    Lab2

---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+$fio = $_POST['fio'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+
+echo "ФИО: ".$fio."<br>Возраст: ".$age."<br>E-mail: ".$email."<br>Телефон: ".$tel."<br>";
+?>
